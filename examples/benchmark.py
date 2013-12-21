2bfc37bcf47c73ac30d9a156087d05418ad44d2d
danielbr <danielbradham@gmail.com>
2013-12-20 23:33:20 -0500

    update benchmark

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -7,6 +7,11 @@ import maya.cmds as cmds
 @manymaya.instance
 def create_lite_scene(filepath):
     #Create a new scene file
+    cmds.scriptEditorInfo(
+        suppressInfo=True,
+        suppressResults=True,
+        suppressErrors=True,
+        suppressWarnings=True)
     cmds.file(new=True, force=True)
 
     #Perform some actions in the new scene
@@ -21,7 +26,11 @@ def create_lite_scene(filepath):
 @manymaya.instance
 def modify_lite_scene(filepath):
     #Use the maya.cmds module to manipulate Maya
-    cmds.scriptEditorInfo(suppressInfo=True, suppressResults=True)
+    cmds.scriptEditorInfo(
+        suppressInfo=True,
+        suppressResults=True,
+        suppressErrors=True,
+        suppressWarnings=True)
     cmds.file(filepath, open=True, force=True)
     cmds.polyCube(name="my_cuber2")
     cmds.file(save=True, force=True)
@@ -31,77 +40,25 @@ if __name__ == "__main__":
     import tempfile
     import shutil
     import os
-    import timeit
+    import time
 
-    def setup():
+    def setup(num_files):
         tempdir = tempfile.mkdtemp()
-        maya_files = [os.path.join(tempdir, 'file{0:0>4d}.mb'.format(i)) for i in range(100)]
+        maya_files = [os.path.join(tempdir, 'file{0:0>4d}.mb'.format(i)) for i in range(num_files)]
         return tempdir, maya_files
 
+    def create_modify_benchmark(num_processes, num_files):
+        print num_processes, " Process:"
+        tempdir, maya_files = setup(num_files)
 
-    #Time 1 Process
-    print "1 Process:"
-    tempdir, maya_files = setup()
+        start = time.clock()
+        manymaya.start(maya_files, create_lite_scene, processes=num_processes, verbose=True)
+        print "\tCreate: {0}".format(time.clock() - start)
 
-    create_time = timeit.timeit(
-        'manymaya.start(maya_files, create_lite_scene, processes=1, verbose=False)',
-        number=10)
-    print "\tCreate 100 files: {0}".format(create_time)
+        start = time.clock()
+        manymaya.start(maya_files, modify_lite_scene, processes=num_processes, verbose=True)
+        print "\tModify: {0}".format(time.clock() - start)
 
-    modify_time = timeit.timeit(
-        'manymaya.start(maya_files, modify_lite_scene, processes=1, verbose=False)',
-        number=10)
-    print "\tModify 100 files: {0}".format(modify_time)
+        shutil.rmtree(tempdir)
 
-    shutil.rmtree(tempdir)
-
-
-    #Time 2 Process
-    print "2 Process:"
-    tempdir, maya_files = setup()
-
-    create_time = timeit.timeit(
-        'manymaya.start(maya_files, create_lite_scene, processes=2, verbose=False)',
-        number=10)
-    print "\tCreate 100 files: {0}".format(create_time)
-
-    modify_time = timeit.timeit(
-        'manymaya.start(maya_files, modify_lite_scene, processes=2, verbose=False)',
-        number=10)
-    print "\tModify 100 files: {0}".format(modify_time)
-
-    shutil.rmtree(tempdir)
-
-
-    #Time 3 Process
-    print "3 Process:"
-    tempdir, maya_files = setup()
-
-    create_time = timeit.timeit(
-        'manymaya.start(maya_files, create_lite_scene, processes=3, verbose=False)',
-        number=10)
-    print "\tCreate 100 files: {0}".format(create_time)
-
-    modify_time = timeit.timeit(
-        'manymaya.start(maya_files, modify_lite_scene, processes=3, verbose=False)',
-        number=10)
-    print "\tModify 100 files: {0}".format(modify_time)
-
-    shutil.rmtree(tempdir)
-
-
-    #Time 4 Process
-    print "4 Process:"
-    tempdir, maya_files = setup()
-
-    create_time = timeit.timeit(
-        'manymaya.start(maya_files, create_lite_scene, processes=4, verbose=False)',
-        number=10)
-    print "\tCreate 100 files: {0}".format(create_time)
-
-    modify_time = timeit.timeit(
-        'manymaya.start(maya_files, modify_lite_scene, processes=4, verbose=False)',
-        number=10)
-    print "\tModify 100 files: {0}".format(modify_time)
-
-    shutil.rmtree(tempdir)
+    create_modify_benchmark(2, 2000)
